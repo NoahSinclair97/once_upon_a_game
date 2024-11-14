051f3dbae16e3ba4345522535943ede47bf61d0d
Noah Sinclair <nsinclair3@academic.rrc.ca>
2024-11-14 14:21:28 -0600

    Added validations for addresses

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,6 @@
 class Address < ApplicationRecord
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :province, presence: true
+  validates :postal_code, presence: true
 end
