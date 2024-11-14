55660a03eb1db32631804a705488b7403abaedeb
Noah Sinclair <nsinclair3@academic.rrc.ca>
2024-11-14 14:42:56 -0600

    Added validations for cards

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,12 @@
 class Card < ApplicationRecord
+  validates :card_number, presence: true, format: { with: /^(?:
+  (4[0-9]{12}(?:[0-9]{3})?) |          # Visa
+  (5[1-5][0-9]{14}) |                  # MasterCard
+  (6(?:011|5[0-9]{2})[0-9]{12}) |      # Discover
+  (3[47][0-9]{13}) |                   # AMEX
+  (3(?:0[0-5]|[68][0-9])[0-9]{11}) |   # Diners Club
+  ((?:2131|1800|35[0-9]{3})[0-9]{11})  # JCB
+  )$/ }
+  validates :exp_date, presence: true
+  validates :cvv, presence: true, format: { with: /\d{3}/ }
 end
