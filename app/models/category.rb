602c554538d07e7e03eeb0bafed339bb5b9328a7
Noah Sinclair <nsinclair3@academic.rrc.ca>
2024-11-14 17:43:27 -0600

    Defined ransackable attributes for admin and categories. Added sassc gem

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
   validates :name, presence: true
+  def self.ransackable_attributes(auth_object = nil)
+    ["category_id", "created_at", "id", "name", "updated_at"]
+  end
 end
