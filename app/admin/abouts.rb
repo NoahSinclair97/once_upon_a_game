38f72047b32fff5d29183d09d249c35bd1ccbd84
Noah Sinclair <nsinclair3@academic.rrc.ca>
2024-11-15 15:40:58 -0600

    Added ransackable attributes to about model and permited params in active admin

--- app/admin/abouts.rb
+++ app/admin/abouts.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register About do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :info
+  permit_params :info
   #
   # or
   #
@@ -14,5 +14,5 @@ ActiveAdmin.register About do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
+
 end
--- app/models/about.rb
+++ app/models/about.rb
@@ -1,2 +1,5 @@
 class About < ApplicationRecord
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "id", "info", "updated_at"]
+  end
 end
