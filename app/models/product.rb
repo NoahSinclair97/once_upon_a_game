250c60fa74f8ff4974c22a29db9904187e50dd6d
Noah Sinclair <nsinclair3@academic.rrc.ca>
2024-11-14 18:06:40 -0600

    Added Products to active admin. Defined ransackable attributes for products.

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,10 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :price, presence: true
   validates :stock, presence: true
+  def self.ransackable_associations(auth_object = nil)
+    ["category"]
+  end
+  def self.ransackable_attributes(auth_object = nil)
+    ["category_id", "created_at", "description", "id", "name", "price", "stock", "updated_at"]
+  end
 end
