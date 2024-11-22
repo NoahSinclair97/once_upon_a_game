c8cd5e8785bd597fd34ec9f45868cc4c85ec033c
Noah Sinclair <nsinclair3@academic.rrc.ca>
2024-11-21 19:21:46 -0600

    Configured partials for products pages

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < InheritedResources::Base
     def index
-      @products = Product.page(params[:page]).per(10)
+      @products = Product.page(params[:page]).per(12)
     end
 
     def show
@@ -10,9 +10,9 @@ class ProductsController < InheritedResources::Base
     def search
       @product_query = Product.ransack(params[:q])
       if params[:category] == ""
-        @product_result = @product_query.result
+        @product_result = @product_query.result.page(params[:page]).per(12)
       else
-        @product_result = @product_query.result.where(category_id: params[:category])
+        @product_result = @product_query.result.where(category_id: params[:category]).page(params[:page]).per(12)
       end
       @categories = Category.all()
     end
